83ade519474f27a267afd428c60aa4441f6a1dae
callum_linden <none@none>
2016-02-23 16:53:57 -0800

    DRTVRW-418 Initial traunch of changes for Windows/Windows64 builds - completes successfully locally - yet to test in TC

--- build-cmd.sh
+++ build-cmd.sh
@@ -29,49 +29,42 @@ build=${AUTOBUILD_BUILD_ID:=0}
 echo "${GLOD_VERSION}.${build}" > "${STAGING_DIR}/VERSION.txt"
 
 case "$AUTOBUILD_PLATFORM" in
-    "windows")
-        build_sln "glodlib.sln" "Debug|Default"
-        build_sln "glodlib.sln" "Release|Default"
-		mkdir -p stage/lib/{debug,release}
-        cp "lib/debug/glod.lib" \
-            "stage/lib/debug/glod.lib"
-        cp "lib/debug/glod.dll" \
-            "stage/lib/debug/glod.dll"
-        cp "src/api/debug/glod.pdb" \
-            "stage/lib/debug/glod.pdb"
-        cp "lib/release/glod.lib" \
-            "stage/lib/release/glod.lib"
-        cp "lib/release/glod.dll" \
-            "stage/lib/release/glod.dll"
+    windows*)
+        build_sln "glodlib.sln" "Release|$AUTOBUILD_WIN_VSPLATFORM"
+
+        mkdir -p stage/lib/release
+
+        cp "lib/release/glod.lib" "stage/lib/release/glod.lib"
+        cp "lib/release/glod.dll" "stage/lib/release/glod.dll"
     ;;
-        "darwin")
-			libdir="$top/stage/lib"
-            mkdir -p "$libdir"/{debug,release}
-			make -C src clean
-			make -C src debug
-			install_name_tool -id "@executable_path/../Resources/libGLOD.dylib" "lib/libGLOD.dylib" 
-			cp "lib/libGLOD.dylib" \
-				"$libdir/debug/libGLOD.dylib"
-			make -C src clean
-			make -C src release
-			install_name_tool -id "@executable_path/../Resources/libGLOD.dylib" "lib/libGLOD.dylib" 
-			cp "lib/libGLOD.dylib" \
-				"$libdir/release/libGLOD.dylib"
-		;;
-        "linux")
-			libdir="$top/stage/lib"
-            mkdir -p "$libdir"/{debug,release}
-			export CFLAGS=-m32
-			export LFLAGS=-m32
-			make -C src clean
-			make -C src debug
-			cp "lib/libGLOD.so" \
-				"$libdir/debug/libGLOD.so"
-			make -C src clean
-			make -C src release
-			cp "lib/libGLOD.so" \
-				"$libdir/release/libGLOD.so"
-        ;;
+    "darwin")
+        libdir="$top/stage/lib"
+        mkdir -p "$libdir"/{debug,release}
+        make -C src clean
+        make -C src debug
+        install_name_tool -id "@executable_path/../Resources/libGLOD.dylib" "lib/libGLOD.dylib" 
+        cp "lib/libGLOD.dylib" \
+            "$libdir/debug/libGLOD.dylib"
+        make -C src clean
+        make -C src release
+        install_name_tool -id "@executable_path/../Resources/libGLOD.dylib" "lib/libGLOD.dylib" 
+        cp "lib/libGLOD.dylib" \
+            "$libdir/release/libGLOD.dylib"
+    ;;
+    "linux")
+        libdir="$top/stage/lib"
+        mkdir -p "$libdir"/{debug,release}
+        export CFLAGS=-m32
+        export LFLAGS=-m32
+        make -C src clean
+        make -C src debug
+        cp "lib/libGLOD.so" \
+            "$libdir/debug/libGLOD.so"
+        make -C src clean
+        make -C src release
+        cp "lib/libGLOD.so" \
+            "$libdir/release/libGLOD.so"
+    ;;
 esac
 mkdir -p "stage/include/glod"
 cp "include/glod.h" "stage/include/glod/glod.h"
